44ec3812d39e8ee517b15d82a2c6197b0199df85
228500 <228500@st.takushoku-u.ac.jp>
2023-07-26 13:54:14 +0900

    課題提出

--- weather.js
+++ weather.js
@@ -45,48 +45,104 @@ let data = {
   "cod": 200
 };
 
-////////// 課題3-2 ここからプログラムを書こう
-let A = document.querySelector('#print');
-A.addEventListener('click', greeting);
-function greeting() {
-	let B = document.querySelector('input[name="timei"]');
-	let timei = B.value;
-  console.log(timei);
-
-  let div = document.querySelector('div#result');
-  let a= document.createElement('p'); 
-  a.textContent='・都市名: 北京市';
-  div.insertAdjacentElement('beforeend', a); 
-  div = document.querySelector('div#result');
-  let b = document.createElement('p'); 
-  b.textContent='・緯度: 116.3972';
-  div.insertAdjacentElement('beforeend', b); 
-  div= document.querySelector('div#result');
-  let c = document.createElement('p'); 
-  c.textContent='・経度: 39.9075';
-  div.insertAdjacentElement('beforeend', c); 
-  div= document.querySelector('div#result');
-  let d = document.createElement('p'); 
-  d.textContent='・天気: 曇りがち';
-  div.insertAdjacentElement('beforeend', d); 
-  div= document.querySelector('div#result');
-  let e = document.createElement('p'); 
-  e.textContent='・最低気温: 9.94';
-  div.insertAdjacentElement('beforeend', e); 
-  div = document.querySelector('div#result');
-  let f = document.createElement('p'); 
-  f.textContent='・最高気温: 9.94';
-  div.insertAdjacentElement('beforeend', f); 
-  div = document.querySelector('div#result');
-  let g = document.createElement('p'); 
-  g.textContent='・湿度: 14';
-  div.insertAdjacentElement('beforeend', g); 
-  div = document.querySelector('div#result');
-  let h = document.createElement('p'); 
-  h.textContent='・風速: 2.65';
-  div.insertAdjacentElement('beforeend', h); 
-  div = document.querySelector('div#result');
-  let i= document.createElement('p'); 
-  i.textContent='・風向: 197';
-  div.insertAdjacentElement('beforeend', i); 
+let B = document.querySelector('#print');
+B.addEventListener('click', sendRequest);
+function sendRequest() {
+  let s = document.querySelector('select#timei');
+  let i = s.selectedIndex;
+  let id;
+  if(i===1){
+    id=360630;
+  }else if(i===2){
+    id=524901;
+  }else if(i===3){
+    id=993800;
+  }else if(i===4){
+    id=1816670;
+  }else if(i===5){
+    id=1850147;
+  }else if(i===6){
+    id=1880252;
+  }else if(i===7){
+    id=2147714;
+  }else if(i===8){
+    id=2643743;
+  }else if(i===9){
+    id=2968815;
+  }else if(i===10){
+    id=3451189;
+  }else if(i===11){
+    id=5128581;
+  }else if(i===12){
+    id=5368361;
+  }
+  let url='https://www.nishita-lab.org/web-contents/jsons/openweather/'+ id +'.json';
+  axios.get(url)
+      .then(showResult)   
+      .catch(showError)   
+      .then(finish);      
 }
+let div = document.querySelector('div#result');
+let a= document.createElement('p'); 
+a.textContent='緯度: ';
+div.insertAdjacentElement('beforeend', a); 
+div = document.querySelector('div#result');
+let b = document.createElement('p'); 
+b.textContent='経度: ';
+div.insertAdjacentElement('beforeend', b); 
+div= document.querySelector('div#result');
+let c = document.createElement('p'); 
+c.textContent='天気: ';
+div.insertAdjacentElement('beforeend', c); 
+div= document.querySelector('div#result');
+let d = document.createElement('p'); 
+d.textContent='最低気温: ';
+div.insertAdjacentElement('beforeend', d);
+div= document.querySelector('div#result');
+let e = document.createElement('p'); 
+e.textContent='最高気温: ';
+div.insertAdjacentElement('beforeend', e);
+div = document.querySelector('div#result');
+let f = document.createElement('p'); 
+f.textContent='湿度: ';
+div.insertAdjacentElement('beforeend', f); 
+div = document.querySelector('div#result');
+let g = document.createElement('p'); 
+g.textContent='風速: ';
+div.insertAdjacentElement('beforeend', g); 
+div = document.querySelector('div#result');
+let h = document.createElement('p'); 
+h.textContent='風向: ';
+div.insertAdjacentElement('beforeend', h); 
+div = document.querySelector('div#result');
+let i= document.createElement('p'); 
+i.textContent='都市名: ';
+div.insertAdjacentElement('beforeend', i); 
+function showResult(resp) {
+  let data = resp.data;
+  if (typeof data === 'string') {
+      data = JSON.parse(data);
+  }
+  a.textContent='緯度: '+data.coord.lon;
+  b.textContent='経度: '+data.coord.lat;
+  c.textContent='天気: '+data.weather[0].description;
+  d.textContent='最低気温: '+data.main.temp_min;
+  e.textContent='最高気温: '+data.main.temp_max;
+  f.textContent='湿度: '+data.main.humidity;
+  g.textContent='風速: '+data.wind.speed;
+  h.textContent='風向: '+data.wind.deg;
+  i.textContent='都市名: '+data.name;
+}
+function showError(err) {
+  console.log(err);
+}
+function finish() {
+  console.log('Ajax 通信が終わりました');
+}
+///let A = document.querySelector('#print');
+///A.addEventListener('click', greeting);
+///function greeting() {
+	///let B = document.querySelector('input[name="timei"]');
+	///let timei = B.value;
+  ///console.log(timei);
+///}
